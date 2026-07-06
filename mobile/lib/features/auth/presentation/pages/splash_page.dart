d627cdfff2f47c76dfb1e869f84fab2758ca3d95
mjrajput04 <manojvagadiya04@gmail.com>
2026-07-06 15:17:53 +0530

    Refactor code structure for improved readability and maintainability

--- mobile/lib/features/auth/presentation/pages/splash_page.dart
+++ mobile/lib/features/auth/presentation/pages/splash_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../../core/theme/app_theme.dart';
-import '../../../../core/widgets/app_logo.dart';
 import '../bloc/auth_bloc.dart';
 
 class SplashPage extends StatelessWidget {
@@ -19,50 +16,11 @@ class SplashPage extends StatelessWidget {
           context.go('/welcome');
         }
       },
-      child: Scaffold(
-        backgroundColor: AppColors.primary,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(28.r),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.15),
-                      blurRadius: 30,
-                      offset: const Offset(0, 10),
-                    ),
-                  ],
-                ),
-                child: AppLogo(size: 100.w, radius: 28.r),
-              ),
-              SizedBox(height: 24.h),
-              Text(
-                'Gora Cabs',
-                style: TextStyle(
-                  fontSize: 32.sp,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                  fontFamily: 'Poppins',
-                ),
-              ),
-              SizedBox(height: 8.h),
-              Text(
-                'Taxi Requirement Network',
-                style: TextStyle(
-                  fontSize: 14.sp,
-                  color: Colors.white70,
-                  fontFamily: 'Poppins',
-                ),
-              ),
-              SizedBox(height: 60.h),
-              CircularProgressIndicator(
-                color: Colors.white,
-                strokeWidth: 2,
-              ),
-            ],
+      child: const Scaffold(
+        body: SizedBox.expand(
+          child: Image(
+            image: AssetImage('assets/images/loding_screen.jpeg'),
+            fit: BoxFit.cover,
           ),
         ),
       ),
