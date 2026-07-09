2c85169135ff0aa0dac8299d5d18f60592d1b957
mjrajput04 <manojvagadiya04@gmail.com>
2026-07-09 12:12:28 +0530

    Refactor code structure for improved readability and maintainability

--- mobile/lib/features/referral/presentation/pages/invite_page.dart
+++ mobile/lib/features/referral/presentation/pages/invite_page.dart
@@ -47,7 +47,7 @@ class _InvitePageState extends State<InvitePage> {
   }
 
   String get _shareText =>
-      'Join me on Gora Texi Partner! 🚕\nUse my referral code *$_code* when you sign up.\nDownload the app: https://play.google.com/store/apps/details?id=com.taxi.call_taxi_partner';
+      'Join me on Gora Taxi Partner! 🚕\nUse my referral code *$_code* when you sign up.\nDownload the app: https://play.google.com/store/apps/details?id=com.taxi.call_taxi_partner';
 
   void _copy() {
     Clipboard.setData(ClipboardData(text: _code));
